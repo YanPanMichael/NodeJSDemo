4c7200d7df6d67b09c984da1b1a6dcc7c29d9027
YanPanMichael <yanpancsu@icloud.com>
2019-05-09 00:46:36 +0800

    save

--- 02_url.js
+++ 02_url.js
@@ -20,25 +20,13 @@ http.createServer(function(req,res){
 
 //req.url  获取浏览器url输入的信息
 
-
-
-
-
-    //var query=url.parse(req.url,true);
-    //
-    //
-    //console.log(query);
-
-
     res.writeHead(200,{"Content-Type":"text/html;charset='utf-8'"});
-
-
 
     if(req.url!='/favicon.ico'){
         //http://localhost:8001/news?aid=123
         //console.log(req.url);  //返回  /news?aid=123
 
-        var result=url.parse(req.url,true);  //第一个参数是地址    第二个参数是true的话表示把get传值转换成对象
+        var result=url.parse(req.url, true);  //第一个参数是地址    第二个参数是true的话表示把get传值转换成对象
 
         console.log('aid='+result.query.aid);  /*获取url的get传值*/
 
