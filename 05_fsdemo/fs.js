0a12d22a6efbf454557a86939bf8fbca2b979892
YanPanMichael <yanpancsu@icloud.com>
2019-05-11 22:53:09 +0800

    nodejs fs demo

---
+++ 05_fsdemo/fs.js
@@ -0,0 +1,16 @@
+var fs = require('fs');
+
+fs.readdir('demo', (err, dir) => {
+  if(!err) {
+    console.log(dir);
+    for (let index = 0; index < dir.length; index++) {
+      const element = dir[index];
+      console.log(element);
+      fs.stat(dir[index], (err, stat) => {
+        if(stat.isDirectory) {
+          console.log(dir[index]);
+        }
+      })
+    }
+  }
+})
