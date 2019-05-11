51946d9f056158d10f21dfbb3d39a5a03d1103e6
YanPanMichael <yanpancsu@icloud.com>
2019-05-11 23:29:56 +0800

    error 2

--- 05_fsdemo/fs.js
+++ 05_fsdemo/fs.js
@@ -3,15 +3,22 @@ var fs = require('fs');
 fs.readdir('demo', (err, dir) => {
   if(!err) {
     console.log(dir);
-    for (let index = 0; index < dir.length; index++) {
+    var resArr = [];
+    //不用高级方法表达式，使用回调函数
+    (function loop(index){
+      if(index === dir.length) {
+        console.log(resArr, resArr);
+        return false;
+      }
       const element = dir[index];
       console.log('element',element);
-
-      fs.stat('demo/'+dir[index], (err, stat) => {
+      fs.stat('demo/'+dir[index], function(err, stat) {
         if(stat.isDirectory()) {
-          console.log('stat',dir[index]);
+          console.log('stat', dir[index]);
+          resArr.push(dir[index]);
         }
+        loop(index+1);
       })
-    }
+    })(0)
   }
 })
